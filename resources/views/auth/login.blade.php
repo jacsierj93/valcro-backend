05463a418267e3e912813bf31f414cff49e787c5
luis D'Lima <delimce@gmail.com>
2016-03-26 13:54:14 -0430

    cambiando mensajes del login y aceptar enter en el formulario

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,58 +5,55 @@
 @endsection
 
 @section('content')
-<body class="hold-transition login-page">
+    <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
             <img src="{{ url('/img/valcro/logo_inicio.png') }}"/>
         </div><!-- /.login-logo -->
 
 
-    <div class="login-box-body">
-    <p class="login-box-msg">Inicia una Sesión como usuario</p>
-    <form name="form1" id="form1">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="Usuario" name="usuario"/>
-            <span class="glyphicon glyphicon-user form-control-feedback"></span>
-        </div>
-        <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Password" name="password"/>
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-        </div>
-        <div class="row">
-           {{--
-            <div class="col-xs-8">
-                <div class="checkbox icheck">
-                    <label>
-                        <input type="checkbox" name="remember"> Remember Me
-                    </label>
+        <div class="login-box-body">
+            <p class="login-box-msg">Inicia una Sesión como usuario</p>
+            <form name="form1" id="form1">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" placeholder="Usuario" name="usuario"/>
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
                 </div>
-            </div>--}}
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-control" placeholder="Password" name="password"/>
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                </div>
+                <div class="row">
+                     <div id="message">&nbsp;</div>
+                    <!-- /.col -->
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
+                    </div><!-- /.col -->
+                </div>
+            </form>
 
-            <!-- /.col -->
-            <div class="col-xs-4">
-                <a href="javascript:userLogin()" class="btn btn-primary btn-block btn-flat">Entrar</a>
-            </div><!-- /.col -->
-        </div>
-    </form>
 
-    {{--@include('auth.partials.social_login')--}}
+        </div><!-- /.login-box-body -->
 
-{{--
-    <a href="{{ url('/password/reset') }}">I forgot my password</a><br>
-    <a href="{{ url('/register') }}" class="text-center">Register a new membership</a>
---}}
-
-</div><!-- /.login-box-body -->
-
-</div><!-- /.login-box -->
+    </div><!-- /.login-box -->
 
     @include('layouts.partials.scripts')
 
 
     <script src="{{url("js/module/account.js")}}"></script>
 
-</body>
+    <script>
+
+        $("#form1").submit(function (e) {
+            userLogin();
+            e.preventDefault(); // avoid to execute the actual submit of the form.
+        });
+
+
+    </script>
+
+
+    </body>
 
 @endsection
