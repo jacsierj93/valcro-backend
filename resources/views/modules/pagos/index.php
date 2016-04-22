bbbc1640dd709fbd006a501769d9900da99a846e
Miguel E. Quevedo H <mquevedo.sistema@valcro.co>
2016-04-22 16:36:58 -0430

    Agrgado camps del grid

--- resources/views/modules/pagos/index.php
+++ resources/views/modules/pagos/index.php
@@ -28,27 +28,27 @@
                 <div flex>Nombre del proveedor</div>
                 <div layout="row" style="height: 24px;">
                     <div flex layout layout-align="center center">
-                        <div layout layout-align="center center" class="cantFactDeb" style="background-color: green;">
+                        <div layout layout-align="center center" class="cantFactDeb" style="background-color: #003000;">
                             85
                         </div>
                     </div>
                     <div flex layout layout-align="center center">
-                        <div layout layout-align="center center" class="cantFactDeb" style="background-color: green;">
+                        <div layout layout-align="center center" class="cantFactDeb" style="background-color: #006600;">
                             85
                         </div>
                     </div>
                     <div flex layout layout-align="center center">
-                        <div layout layout-align="center center" class="cantFactDeb" style="background-color: green;">
+                        <div layout layout-align="center center" class="cantFactDeb" style="background-color: #009A00;">
                             85
                         </div>
                     </div>
                     <div flex layout layout-align="center center">
-                        <div layout layout-align="center center" class="cantFactDeb" style="background-color: green;">
+                        <div layout layout-align="center center" class="cantFactDeb" style="background-color: #00CD00;">
                             85
                         </div>
                     </div>
                     <div flex layout layout-align="center center">
-                        <div layout layout-align="center center" class="cantFactDeb" style="background-color: green;">
+                        <div layout layout-align="center center" class="cantFactDeb" style="background-color: #00CCA2;">
                             85
                         </div>
                     </div>
@@ -82,27 +82,37 @@
 
             <md-content class="cntLayerHolder" layout="column" layout-padding flex>
 
-            <form name="projectForm">
+                <form name="projectForm">
 
-                <div class="titulo_formulario" layout="Column" layout-align="start start">
-                    <div>
-                        Pagos Pendientes
-                    </div>
-                </div>
-                <div layout="row" class="headGridHolder">
-                    <div flex class="headGrid"> Encabezado Uno</div><div flex class="headGrid"> Encabezado Dos</div><div flex class="headGrid"> Encabezado Tres</div>
-                </div>
-                <div id="grid">
-                    <div flex>
-                        <div layout="row" class="cellGridHolder">
-                            <div flex class="cellGrid"> Data Uno</div><div flex class="cellGrid"> Data Dos</div><div flex class="cellGrid"> Data Tres</div>
+                    <div class="titulo_formulario" layout="Column" layout-align="start start">
+                        <div>
+                            Pagos Pendientes
                         </div>
                     </div>
-                </div>
+                    <div layout="row" class="headGridHolder">
+                        <div flex class="headGrid"> N° Factura</div>
+                        <div flex class="headGrid"> Emitido</div>
+                        <div flex class="headGrid"> Tipo</div>
+                        <div flex class="headGrid"> Pagado</div>
+                        <div flex class="headGrid"> Resta</div>
+                        <div flex class="headGrid"> 000000</div>
+                    </div>
+                    <div id="grid">
+                        <div flex>
+                            <div layout="row" class="cellGridHolder">
+                                <div flex class="cellGrid"> Data 1</div>
+                                <div flex class="cellGrid"> Data 2</div>
+                                <div flex class="cellGrid"> Data 3</div>
+                                <div flex class="cellGrid"> Data 4</div>
+                                <div flex class="cellGrid"> Data 5</div>
+                                <div flex class="cellGrid"> Data 6</div>
+                            </div>
+                        </div>
+                    </div>
 
-            </form>
+                </form>
 
-                </md-content>
+            </md-content>
         </md-sidenav>
 
 
