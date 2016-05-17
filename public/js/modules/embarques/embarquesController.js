0d2d33ee3305ae5934d971961e5098c06e7962ee
Luis Navarro <luisnavarro.dg@gmail.com>
2016-05-17 12:40:24 -0400

    adelanto alertas, con tabs

--- public/js/modules/embarques/embarquesController.js
+++ public/js/modules/embarques/embarquesController.js
@@ -48,4 +48,40 @@ MyApp.controller('embarquesCtrll', ['$scope', '$mdSidenav', function ($scope, $m
     };
 
 
+    $scope.alerts = {
+        alert: [true, [
+            {title: 'One', content: "Tabs will become paginated if there isn't enough room for them."},
+            {title: 'Two', content: "You can swipe left and right on a mobile device to change tabs."},
+            {title: 'Three', content: "You can bind the selected tab via the selected attribute on the md-tabs element."}
+        ]],
+        error: [true, [{title: 'One', content: "Tabs will become paginated if there isn't enough room for them."}]],
+        info: [true, [{title: 'Three', content: "You can bind the selected tab via the selected attribute on the md-tabs element."}]]
+    };
+
+    $scope.addTab = function (title, view) {
+        view = view || title + " Content View";
+        tabs.push({title: title, content: view, disabled: false});
+    };
+
+    console.log($scope.alerts);
+
+
+    var tabs = [
+            {title: 'One', content: "Tabs will become paginated if there isn't enough room for them."},
+            {title: 'Two', content: "You can swipe left and right on a mobile device to change tabs."},
+            {title: 'Three', content: "You can bind the selected tab via the selected attribute on the md-tabs element."}
+        ],
+        selected = null,
+        previous = null;
+    $scope.tabs = tabs;
+    $scope.selectedIndex = 2;
+
+    $scope.$watch('selectedIndex', function (current, old) {
+        previous = selected;
+        selected = tabs[current];
+        if (old + 1 && (old != current)) $log.debug('Goodbye ' + previous.title + '!');
+        if (current + 1)                 $log.debug('Hello ' + selected.title + '!');
+    });
+
+
 }]);
