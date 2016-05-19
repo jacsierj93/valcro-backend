3fcc38962e97b99f53683355adb4c415671de8ac
Luis Navarro <luisnavarro.dg@gmail.com>
2016-05-19 12:17:40 -0400

    alerts finalizados v1.0

--- public/js/modules/embarques/embarquesController.js
+++ public/js/modules/embarques/embarquesController.js
@@ -1,4 +1,4 @@
-MyApp.controller('embarquesCtrll', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
+MyApp.controller('embarquesCtrll', ['$scope', '$mdSidenav','setNotif', function ($scope, $mdSidenav,setNotif) {
 
     var historia = [15];
     $scope.index = index = 0;
@@ -48,40 +48,123 @@ MyApp.controller('embarquesCtrll', ['$scope', '$mdSidenav', function ($scope, $m
     };
 
 
-    $scope.alerts = {
-        alert: [true, [
-            {title: 'One', content: "Tabs will become paginated if there isn't enough room for them."},
-            {title: 'Two', content: "You can swipe left and right on a mobile device to change tabs."},
-            {title: 'Three', content: "You can bind the selected tab via the selected attribute on the md-tabs element."}
-        ]],
-        error: [true, [{title: 'One', content: "Tabs will become paginated if there isn't enough room for them."}]],
-        info: [true, [{title: 'Three', content: "You can bind the selected tab via the selected attribute on the md-tabs element."}]]
+    $scope.alertar = function(){
+        $mdSidenav('lyrAlert').open();
+    }
+    // ####################################################################################################
+    $scope.alerts = setNotif.listNotif();
+
+    $scope.ok = function(call){
+        call.opc.action();
+    }
+
+    $scope.closeThis = function(target){
+      $scope.alerts[target].splice($scope.selected[target],1);
     };
 
-    $scope.addTab = function (title, view) {
-        view = view || title + " Content View";
-        tabs.push({title: title, content: view, disabled: false});
+    $scope.callInfo = function(){
+        setNotif.addNotif('info','prueba111','Mensajeee de prueba con texto ramdom'+count,[{name:'pushMe',action:function(){console.log('thanks')}},{name:'dont pushMe',action:function(){console.log('WHY?? :(')}}]);
+        count ++;
+    };
+    var count =0;
+    $scope.callOk = function(){
+        setNotif.addNotif('ok','pruebaOK','ad doloren ipsun :) '+count,[{name:'me! me!',action:function(){console.log('wiiii')}},{name:'nos',action:function(){console.log('hahah')}}]);
+        count++;
+    };
+    $scope.callAdv = function(){
+        setNotif.addNotif('alert','pruebaOK','ad doloren ipsun :) '+count,[{name:'me! me!',action:function(){console.log('wiiii')}},{name:'nos',action:function(){console.log('hahah')}}]);
+        count++;
+    };
+    $scope.callErr = function(){
+        setNotif.addNotif('error','pruebaOK','ad doloren ipsun :) '+count,[{name:'me! me!',action:function(){console.log('wiiii')}},{name:'nos',action:function(){console.log('hahah')}}]);
+        count++;
+    };
+    // ========================================================================
+    // ========================================================================
+    $scope.selected = {alert: 0, error: 0, info: 0, ok: 0};
+    // ========================================================================
+    $scope.alertNext = function (obj) {
+        var total = $scope.alerts[obj].length - 1;
+        var actual = $scope.selected[obj];
+        if (total == actual) {
+            $scope.selected[obj] = 0;
+        } else {
+            $scope.selected[obj] = actual + 1;
+        }
+    };
+    // ========================================================================
+    $scope.alertPrev = function (obj) {
+        var total = $scope.alerts[obj].length - 1;
+        var actual = $scope.selected[obj];
+        if (actual == 0) {
+            $scope.selected[obj] = total;
+        } else {
+            $scope.selected[obj] = actual - 1;
+        }
     };
 
-    console.log($scope.alerts);
+    /*$scope.$watch('alerts', function (current, old) {
+        console.log(current);
+    });*/
 
+    $scope.$watchGroup(['alerts.ok.length','alerts.alert.length','alerts.error.length','alerts.info.length'], function(newValues) {
+        // newValues array contains the current values of the watch expressions
+        // with the indexes matching those of the watchExpression array
+        // i.e.
+        // newValues[0] -> $scope.foo
+        // and
+        // newValues[1] -> $scope.bar
+        console.log(newValues)
 
-    var tabs = [
-            {title: 'One', content: "Tabs will become paginated if there isn't enough room for them."},
-            {title: 'Two', content: "You can swipe left and right on a mobile device to change tabs."},
-            {title: 'Three', content: "You can bind the selected tab via the selected attribute on the md-tabs element."}
-        ],
-        selected = null,
-        previous = null;
-    $scope.tabs = tabs;
-    $scope.selectedIndex = 2;
+        var open = false;
+        angular.forEach(newValues, function(value, key) {
+            if(value > 0 ){
+                open = true;
+                $mdSidenav('lyrAlert').open();
+                return false;
+            }
+            if(key == newValues.length-1 && !open){
+                $mdSidenav('lyrAlert').close();
+            }
+        });
 
-    $scope.$watch('selectedIndex', function (current, old) {
-        previous = selected;
-        selected = tabs[current];
-        if (old + 1 && (old != current)) $log.debug('Goodbye ' + previous.title + '!');
-        if (current + 1)                 $log.debug('Hello ' + selected.title + '!');
+        //console.log(newValues);
+
     });
 
 
+    /*$scope.$watch('selectedIndex', function (current, old) {
+     previous = selected;
+     selected = tabs[current];
+     if (old + 1 && (old != current)) $log.debug('Goodbye ' + previous.title + '!');
+     if (current + 1)                 $log.debug('Hello ' + selected.title + '!');
+     });*/
+
+
 }]);
+
+MyApp.service("setNotif",function(){
+    var d = new Date();
+    var list =  {
+            ok: [],
+            alert: [],
+            error: [],
+            info: []
+    };
+   return {
+       listNotif : function(){
+           return list;
+       },
+       addNotif : function(obj,title, view,opcs){
+           list[obj].unshift({title: title, content: view, opcs: opcs});
+
+       }
+   }
+
+});
+
+MyApp.controller("test",function($scope,setNotif) {
+    $scope.outside = function(){
+        setNotif.addNotif('info','prueba111','llamada desde otro controller',[{name:'pushMe',action:function(){alert("desde otro controllador")}},{name:'dont pushMe',action:function(){console.log('WHY?? :(')}}]);
+    }
+});
--- resources/views/modules/embarques/index.php
+++ resources/views/modules/embarques/index.php
@@ -1,28 +1,91 @@
 <!-- 1) ########################################## CONTENEDOR GENERAL DE LA SECCION ########################################## -->
 <div layout="column" class="md-whiteframe-1dp" flex ng-controller="embarquesCtrll">
-    <span ng-click="openLayer('lyrAlert')"> ABRIRRRR </span>
+    <div ng-controller="test">
+        <span ng-click="outside()"> ABRIRRRR </span>
+    </div>
 
-    <md-sidenav layout="row" style="top: calc(100% - 128px); height: 80px; margin-bottom:48px; width: calc(100% - 288px);" class="md-sidenav-right" md-disable-backdrop="true" md-component-id="lyrAlert" id="lyrAlert">
-        <div flex ng-show="alerts.alert[0]">
-            <md-tabs style="background-color: yellow;" flex>
-                <md-tab label="{{tab.title}}" ng-repeat="tab in alerts.alert[1]">
-                    {{tab.content}}
+
+    <span ng-click="callInfo()"> NUEVA </span>
+    <span ng-click="callOk()"> NUEVA2 </span>
+    <span ng-click="callErr()"> NUEVA3 </span>
+    <span ng-click="callAdv()"> NUEVA4 </span>
+    <md-sidenav layout="row" style="top: calc(100% - 144px); height: 96px; margin-bottom:48px; width: calc(100% - 288px);" class="md-sidenav-right" md-disable-backdrop="true" md-component-id="lyrAlert" id="lyrAlert">
+        <!-- OK   ############################################################################################## -->
+        <div class="alertBox alertOkColor" flex ng-show="alerts.ok.length > 0" layout="row">
+            <div class="alertPrevArrow" ng-click="alertPrev('ok')" ng-show="alerts.ok.length > 1"></div>
+            <md-tabs class="alertContainer" layout="column" md-selected="selected.ok" flex>
+                <md-tab label="{{tab.title}}" layout="column" class="alertItem" flex ng-repeat="tab in alerts.ok">
+                    <div class="alertTextContent" style="">
+                        {{tab.content}}
+                    </div>
+                    <div class="alertTextOpcs" layout="row">
+                        <div flex ng-repeat="opc in tab.opcs" ng-click="closeThis('ok');ok(this)" >
+                            {{opc.name}}
+                        </div>
+                    </div>
                 </md-tab>
             </md-tabs>
+            <div class="alertNextArrow" ng-click="alertNext('ok')" ng-show="alerts.ok.length > 1" style="width: 50px;">
+                <div  ng-show="alerts.ok.length>1">{{selected.ok + 1}}/{{alerts.ok.length}}</div>
+            </div>
         </div>
-        <div flex ng-show="alerts.error[0]">
-            <md-tabs style="background-color: #ac2925;" flex>
-                <md-tab label="{{tab.title}}" ng-repeat="tab in alerts.error[1]">
-                    {{tab.content}}
+        <!-- ALERT ############################################################################################## -->
+        <div class="alertBox alertAlertColor" flex ng-show="alerts.alert.length > 0" layout="row">
+            <div class="alertPrevArrow" ng-click="alertPrev('alert')" ng-show="alerts.alert.length > 1"></div>
+            <md-tabs class="alertContainer" layout="column" md-selected="selected.alert" flex>
+                <md-tab label="{{tab.title}}" layout="column" class="alertItem" ng-repeat="tab in alerts.alert">
+                    <div class="alertTextContent" style="">
+                        {{tab.content}}
+                    </div>
+                    <div class="alertTextOpcs" layout="row">
+                        <div flex ng-repeat="opc in tab.opcs" ng-click="closeThis('alert');ok(this)">
+                            {{opc.name}}
+                        </div>
+                    </div>
                 </md-tab>
             </md-tabs>
+            <div class="alertNextArrow" ng-click="alertNext('alert')" ng-show="alerts.alert.length > 1" style="width: 50px;">
+                <div  ng-show="alerts.alert.length>1">{{selected.alert + 1}}/{{alerts.alert.length}}</div>
+            </div>
         </div>
-        <div flex ng-show="alerts.info[0]">
-            <md-tabs style="background-color: transparent;" flex>
-                <md-tab label="{{tab.title}}" ng-repeat="tab in alerts.info[1]">
-                    {{tab.content}}
+        <!-- ERROR ############################################################################################## -->
+        <div class="alertBox alertErrorColor" flex ng-show="alerts.error.length > 0" layout="row">
+            <div class="alertPrevArrow" ng-click="alertPrev('error')" ng-show="alerts.error.length > 1"></div>
+            <md-tabs class="alertContainer"  md-selected="selected.error" flex>
+                <md-tab label="{{tab.title}}" class="alertItem" ng-repeat="tab in alerts.error">
+                    <div class="alertTextContent" style="">
+                        {{tab.content}}
+                    </div>
+                    <div class="alertTextOpcs" layout="row">
+                        <div flex ng-repeat="opc in tab.opcs" ng-click="closeThis('error');ok(this)">
+                            {{opc.name}}
+                        </div>
+                    </div>
                 </md-tab>
             </md-tabs>
+            <div class="alertNextArrow" ng-click="alertNext('error')" ng-show="alerts.error.length > 1" style="width: 50px;">
+                <div  ng-show="alerts.error.length>1">{{selected.error + 1}}/{{alerts.error.length}}</div>
+            </div>
+        </div>
+        <!-- INFO ############################################################################################## -->
+        <div class="alertBox alertInfoColor" flex ng-show="alerts.info.length > 0" layout="row">
+            <div class="alertPrevArrow" ng-click="alertPrev('info')" ng-show="alerts.info.length > 1"></div>
+            <md-tabs class="alertContainer" md-selected="selected.info" flex>
+                <md-tab label="{{tab.title}}" class="alertItem" ng-repeat="tab in alerts.info">
+                    <div class="alertTextContent" style="">
+                        {{tab.content}}
+                    </div>
+                    <div class="alertTextOpcs" layout="row">
+                        <div flex ng-repeat="opc in tab.opcs" ng-click="closeThis('info');ok(this)" >
+                            {{opc.name}}
+                        </div>
+                    </div>
+
+                </md-tab>
+            </md-tabs>
+            <div class="alertNextArrow" ng-click="alertNext('info')" ng-show="alerts.info.length > 1" style="width: 50px;">
+                <div  ng-show="alerts.info.length>1">{{selected.info + 1}}/{{alerts.info.length}}</div>
+            </div>
         </div>
     </md-sidenav>
 
